5f9f2ccd386175a9bb718515799f22f66804152e
Frank Xu <frankxu973@gmail.com>
2024-02-22 19:45:14 -0500

    commit changes on feb 22

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import mysql.connector
+import random
+import math
 first='a'
 last='b'
 id=1
@@ -8,20 +10,14 @@ try:
     cnx = mysql.connector.connect(host='localhost', user='root', password='')
     cursor = cnx.cursor(buffered=True)
     cursor.execute('USE sakila')
-    query = f'SELECT title, rental_date, return_date\
-        FROM rental AS r JOIN inventory AS i ON r.inventory_id=i.inventory_id\
-        JOIN film AS f ON i.film_id=f.film_id\
-        WHERE customer_id = {int(id)};'
+    query = f'SELECT DISTINCT r.inventory_id FROM inventory AS i, rental AS r\
+        WHERE i.inventory_id=r.inventory_id AND film_id=1 AND r.inventory_id\
+        NOT IN (SELECT DISTINCT r.inventory_id FROM inventory AS i JOIN rental AS r\
+        WHERE i.inventory_id=r.inventory_id AND film_id=1 AND return_date IS NULL) LIMIT 1'
     cursor.execute(query)
-    result = list(cursor.fetchall())
-    for i in result:
-        modify_list = list(i)
-        modify_list[1] = modify_list[1].strftime("%Y-%m-%d %H:%M:%S")
-        modify_list[2] = modify_list[2].strftime("%Y-%m-%d %H:%M:%S")
-        i = tuple(modify_list)
-        
-    for i in result:
-        print(i)
+    result = cursor.fetchall()
+    print(result[0][0])
+    cursor.close()
 except mysql.connector.Error as err:
     print(err)
 finally:
