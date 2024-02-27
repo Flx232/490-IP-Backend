86b9b0ea61a200d0a87f1afd2e6bd488617048be
Frank Xu <frankxu973@gmail.com>
2024-02-27 10:30:33 -0500

    commit all changes in feb27

--- test.py
+++ test.py
@@ -10,7 +10,7 @@ try:
     cnx = mysql.connector.connect(host='localhost', user='root', password='')
     cursor = cnx.cursor(buffered=True)
     cursor.execute('USE sakila')
-    query = f'SELECT customer_id FROM customer WHERE email LIKE \"MARY.SMITH@sakilacustomer.org\"'
+    query = f'SELECT address_id FROM address WHERE phone LIKE \'{97311112312}\' AND address NOT LIKE \'34 Main Street\''
     cursor.execute(query)
     result = cursor.fetchall()
     print(len(result))
